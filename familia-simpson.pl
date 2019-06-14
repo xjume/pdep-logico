465004f6b27335eb9c2bd55019f107a225420527
Ju Romero <julieromero@est.frba.utn.edu.ar>
2019-06-13 23:10:00 -0300

    Ejercicio resuelto. Falta bonus.

---
+++ familia-simpson.pl
@@ -0,0 +1,64 @@
+% BASE DE CONOCIMIENTO
+
+padreDe(abe, abbie).
+padreDe(abe, homero).
+padreDe(abe, herbert).
+padreDe(clancy, marge).
+padreDe(clancy, patty).
+padreDe(clancy, selma).
+padreDe(homero, bart).
+padreDe(homero, hugo).
+padreDe(homero, lisa).
+padreDe(homero, maggie).
+
+madreDe(edwina, abbie).
+madreDe(mona, homero).
+madreDe(gaby, herbert).
+madreDe(jacqueline, marge).
+madreDe(jacqueline, patty).
+madreDe(jacqueline, selma).
+madreDe(marge, bart).
+madreDe(marge, hugo).
+madreDe(marge, lisa).
+madreDe(marge,maggie).
+madreDe(selma, ling).
+
+% PREDICADOS PEDIDOS EN CONSIGNA
+tieneHijo(Progenitor) :-
+    padreDe(Progenitor,_).
+
+tieneHijo(Progenitor) :-
+    madreDe(Progenitor,_).
+
+hermanos(Hermano1,Hermano2) :-
+    compartenPadre(Hermano1,Hermano2),
+    compartenMadre(Hermano1,Hermano2).
+    
+compartenPadre(Hermano1,Hermano2) :-
+    padreDe(UnProgenitor,Hermano1),
+    padreDe(UnProgenitor,Hermano2).
+
+compartenMadre(Hermano1,Hermano2) :-
+    madreDe(UnaProgenitora,Hermano1),
+    madreDe(UnaProgenitora,Hermano2).
+
+medioHermanos(Hijo1,Hijo2) :-
+    compartenPadre(Hijo1,Hijo2).
+
+medioHermanos(Hijo1,Hijo2) :-
+    compartenMadre(Hijo1,Hijo2).
+
+tioDe(Tio,Sobrino) :-
+    madreDe(Progenitor,Sobrino),
+    hermanos(Tio,Progenitor).
+
+tioDe(Tio,Sobrino) :-
+    padreDe(Progenitor,Sobrino),
+    hermanos(Tio,Progenitor).
+
+abueloMultiple(Abuelo) :-
+    padreDe(Abuelo,PadreOMadre),
+    tieneHijo(PadreOMadre).
+
+% BONUS
+% descendiente(Descendiente,Ascendiente) :-
