7fe78cc32ef731d898ce56622d534a1a05dae2f9
Ju Romero <julieromero@est.frba.utn.edu.ar>
2019-07-08 15:49:40 -0300

    Resolución correcta de parcial Medicamentos.

--- medicamentos.pl
+++ medicamentos.pl
@@ -11,6 +11,12 @@ estaEnfermo(Persona,Enfermedad).
 padre(Padre,Hijo).
 actividad(Persona,Fecha,Actividad). 
 
+persona(Persona):- estaEnfermo(Persona,_).
+medicamento(Medicamento):- incluye(Medicamento,_).
+droga(Droga):- incluye(_,Droga).
+farmacia(Farmacia):- vende(Farmacia,_,_).
+fecha(Fecha):- actividad(_,Fecha,_).
+
 vende(laGondoriana, trancosin, 35).
 vende(laGondoriana, sanaSam, 35).
 incluye(trancosin, athelas).
@@ -29,58 +35,104 @@ actividad(eomer, fecha(15, 8, 3014), preguntoPor(sanaSam, laGondoriana)).
 actividad(eowyn, fecha(14, 9, 3014), preguntoPor(sanaSam, laGondoriana)).
 
 medicamentoUtil(Persona, Medicamento):-
+  sirveParaCurar(Medicamento,Persona),
+  not(sirveParaPotenciar(Medicamento,Persona)).
+
+sirveParaCurar(Medicamento,Persona):-
+  sirveParaCurar(Medicamento,Persona,_).
+
+sirveParaCurar(Medicamento,Persona,Enfermedad):-
+  sirveParaCurar(Medicamento,Persona,Enfermedad,_).
+
+sirveParaCurar(Medicamento,Persona,Enfermedad,Droga):-
   incluye(Medicamento,Droga),
   estaEnfermo(Persona,Enfermedad),
-  efecto(Droga,cura(Enfermedad)),
-  not(efecto(Droga,potencia(Enfermedad))).
+  efecto(Droga,cura(Enfermedad)).
+
+sirveParaPotenciar(Medicamento,Persona):-
+  incluye(Medicamento,Droga),
+  estaEnfermo(Persona,Enfermedad),
+  efecto(Droga,potencia(Enfermedad)).
 
 medicamentoMilagroso(Persona, Medicamento):-
-  incluye(Medicamento,Droga),
-  forall(estaEnfermo(Persona,Enfermedad),efecto(Droga,cura(Enfermedad))),
-  forall(estaEnfermo(Persona,Enfermedad),not(efecto(Droga,potencia(Enfermedad)))).
+  persona(Persona),           % Lo ligo para que sea para UNA persona.
+  medicamento(Medicamento),   % Lo ligo para que sea para UN medicamento.
+  forall(estaEnfermo(Persona,Enfermedad),sirveParaCurar(Medicamento,Persona,Enfermedad)),
+  not(sirveParaPotenciar(Medicamento,Persona)).
 
 drogaSimpatica(Droga):-
+  droga(Droga),
   findall(Enfermedad,efecto(Droga,cura(Enfermedad)), Enfermedades),
-  Enfermedades >= 4.
+  length(Enfermedades,CantidadEnfermedades),
+  CantidadEnfermedades >= 4,
+  not(efecto(Droga,potencia(_))).
 
 drogaSimpatica(Droga):-
-  estaEnfermo(eomer,EnfermedadEomer)
-  efecto(Droga,cura(EnfermedadEomer)),
-  not(estaEnfermo(eowyn,OtraEnfermedad)),
-  efecto(Droga,cura(OtraEnfermedad)).
+  sirveParaCurar(_,eomer, Enfermedad1, Droga),
+  sirveParaCurar(_,eowyn, Enfermedad2, Droga),
+  Enfermedad1 \= Enfermedad2.
 
 drogaSimpatica(Droga):-
   incluye(Medicamento,Droga),
-  vende(Farmacia,Medicamento,_),
+  vende(_,Medicamento,_),
   not(seVendeAMasDe10(Medicamento)).
 
 seVendeAMasDe10(Medicamento):-
-  incluye(Medicamento,_),
+  medicamento(Medicamento),
   vende(_,Medicamento,Precio),
   Precio > 10.
 
 tipoSuicida(Persona):-
-  forall(actividad(Persona,_,compro(Medicamento,_)), not(medicamentoUtil(Persona,Medicamento)).
+  actividad(Persona,_,compro(Medicamento,_)),
+  not(sirveParaCurar(Medicamento,Persona)),
+  sirveParaPotenciar(Medicamento,Persona).
 
 tipoAhorrativo(Persona):-
-  forall( actividad(Persona,_,compro(Medicamento,Precio)),
-          actividad(Persona,_,preguntoPor(Medicamento,OtroPrecio))),
-  Precio < OtroPrecio.
+  persona(Persona),   % Ligo para que sea inversible (generador).
+  farmacia(Farmacia), % Ligo para que sea para UNA farmacia.
+  forall(actividad(Persona,_,compro(Medicamento,Farmacia)),
+    preguntaPorUnoMasCaroQueEn(Persona,Medicamento,Farmacia)
+  ).
+
+preguntaPorUnoMasCaroQueEn(Persona,Medicamento,Farmacia):-
+  actividad(Persona,_,preguntoPor(Medicamento,OtraFarmacia)),
+  vende(Farmacia,Medicamento,Precio),
+  vende(OtraFarmacia,Medicamento,OtroPrecio),
+  OtroPrecio > Precio.
 
 tipoActivoEn(Persona, Mes, Anio):-
   actividad(Persona,fecha(_,Mes,Anio),_).
 
 diaProductivo(Fecha):-
-  forall(actividad(Persona,Fecha,actividad(_,_,compro(Medicamento,_))),medicamentoUtil(Persona,Medicamento)).
+  fecha(Fecha),    % Genero Fecha válida para que sea inversible.
+  forall(
+    actividad(Persona,Fecha,Actividad),
+    actividadUtil(Persona,Actividad)
+  ).
 
-diaProductivo(Fecha):-
-  forall(actividad(Persona,Fecha,actividad(_,_,preguntoPor(Medicamento,_))),medicamentoUtil(Persona,Medicamento)).
+actividadUtil(Persona,Actividad):-
+  medicamentoDeLaActividad(Actividad,Medicamento),
+  medicamentoUtil(Persona,Medicamento).
 
+medicamentoDeLaActividad(compro(Medicamento,_),Medicamento).
+medicamentoDeLaActividad(preguntaPor(Medicamento,_),Medicamento).
 gastoTotal(Persona, Plata):-
-  findall(Precio,actividad(Persona,_,compro(_,Precio)),Precios),
+  persona(Persona),
+  findall(Precio,precioPorCompra(Persona,Precio),Precios),
   sumlist(Precios,Plata).
+
+precioPorCompra(Persona,Precio):-
+  actividad(Persona,_,compro(Medicamento,Farmacia)),
+  vende(Farmacia,Medicamento,Precio).
 
 zafoDe(Persona, Enfermedad):-
   not(estaEnfermo(Persona,Enfermedad)),
-  padre(Padre,Persona),
-  estaEnfermo(Padre,Enfermedad).
+  ancestro(Ancestro,Persona),
+  estaEnfermo(Ancestro,Enfermedad).
+
+ancestro(Ancestro,Descendiente):-
+  padre(Ancestro,Descendiente).
+
+ancestro(Ancestro,Descendiente):-
+  padre(Ancestro,OtroDescendiente),
+  ancestro(OtroDescendiente,Descendiente).
